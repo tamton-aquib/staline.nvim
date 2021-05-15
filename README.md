152f9dc94af3f542ca5f09db0df37964ceb1ee7c
tamton-aquib <aquibjavedt007@gmail.com>
2021-05-15 16:35:55 +0530

    manual BRANCHING(4) with main

--- README.md
+++ README.md
@@ -20,25 +20,69 @@ A simple statusline for neovim written in lua.
 
 ### Setting up
 
-* Default configuration
+* Configuration
+	```lua
+	require('staline').setup{}
+	```
+* The Default configuration looks something like:
     ```lua
     require('staline').setup {
 		defaults = {
-            leftSeparator   = "",
-            rightSeparator  = "",
-            cool_symbol     = " "  --varies according to OS type.
+			leftSeparator   = "",
+			rightSeparator  = "",
+			line_column     = "[%l/%L] :%c 並%p%% ",
+			fg              = "#000000",  -- The foreground text color.
+			bg              = "none",     -- Default background is transparent.
+			cool_symbol     = " "        -- Change this to override defult OS icon.
 		},
-		getModeColor = {
+		mode_colors = {
 			n = "#2bbb4f",
 			i = "#986fec",
 			c = "#e27d60",
 			v = "#4799eb",
+		},
+		mode_icons = {
+			n = " ",
+			i = " ",
+			c = " ",
+			v = " ",
 		}
     }
     ```
+* Useful config Ideas:
+	> create color value tables yourself to match your current colorscheme.
+	```lua
+	local gruvbox = {
+		n = "#a89985",
+		i = "#84a598",
+		c = "#8fbf7f",
+		v = "#fc802d",
+	}
+	
+	-- Assign mode_colors as this table.
+	require('staline').setup{
+		mode_colors = gruvbox
+	}
+	```
+	> Use non-unicode characters for showing modes.
+	```lua
+	local no_unicode_modes = {
+		n = "N ",
+		i = "I ",
+		c = "C ",
+		v = "V ",
+	}
+
+	-- Assign modes as this table.
+	require('staline').setup{
+		mode_icons = no_unicode_modes
+	}
+
+	-- You could change the seperators too if you want.
+	```
 
 ### Features
-* Lightweight (~100 LOC)
+* Lightweight ( <100 LOC)
 * Fast
 * Unicode current mode info.
 * Shows current git branch if `plenary` is installed. (If you have telescope, you will probably have this.)
--- lua/staline.lua
+++ lua/staline.lua
@@ -1,65 +1,24 @@
 Config = {}
+Tables = require('tables')
 
-local green     = "#2bbb4f"	--> "#6ed57e"
-local violet    = "#986fec"
-local blue      = "#4799eb"	--> "#03353e"
-local yellow    = "#fff94c"	--> "#ffd55b"
-local black     = "#000000"
-local red       = "#e27d60"
+local cmd = vim.api.nvim_command
+Config.defaults = Tables.defaults
+Config.getFileIcon = Tables.getFileIcon
+Config.mode_colors = Tables.mode_colors
+Config.mode_icons = Tables.mode_icons
 
-function system_icon()
-	if vim.fn.has("win32") == 1 then return "者"
-	elseif vim.fn.has("unix") == 1 then return " "
-	elseif vim.fn.has("mac") == 1 then return " "
+function setup(opts)
+	if not opts then opts = {} end
+
+	for k,v in pairs(opts) do
+		for k1,v1 in pairs(opts[k]) do
+			Config[k][k1] = v1
+		end
 	end
-end
 
-Config.defaults = {
-    leftSeparator = "",
-    rightSeparator = "",
-	line_column = "[%l/%L] :%c 並%p%% ",
-	cool_symbol = system_icon()
-}
-
-Config.getModeColor = {
-     ['n']    =  green,
-     ['v']    =  blue,
-     ['V']    =  blue,
-     ['i']    =  violet,
-     ['ic']   =  violet,
-     ['c']    =  red,
-     ['t']    =  yellow,
-     ['r']    =  yellow,
-     ['R']    =  yellow
-}
-
-Config.modes = {
-     ['n']   = ' ',
-     ['v']   = ' ',
-     ['V']   = ' ',
-     ['i']   = ' ',
-     ['ic']  = '',
-     ['c']   = ' ',
-     ['r']   = 'Prompt',
-     ['t']   = 'T',
-     ['R']   = ' ',
-     ['^V']  = ' '
-}
-
-function Config.setup(opts)
-	if not opts.modes then opts.modes = Config.modes end
-	if not opts.defaults then opts.defaults = Config.defaults end
-	if not opts.getModeColor then opts.getModeColor = Config.getModeColor end
-
-	for k, v in pairs(opts.defaults) do Config.defaults[k] = v end
-	for k, v in pairs(opts.getModeColor) do Config.getModeColor[k] = v end
-	for k, v in pairs(opts.modes) do Config.modes[k] = v end
     vim.o.statusline = '%!v:lua.require\'staline\'.get_statusline()'
 end
 
-local cmd = vim.api.nvim_command
--- local modes = require('tables').modes
-local getFileIcon = require('tables').getFileIcon
 
 local lightGrey = "#303030"
 
@@ -87,40 +46,46 @@ function ifNotFound (t, d)
   setmetatable(t, mt)
 end
 
+function call_highlights(modeColor)
+    local fg = Config.defaults.fg
+	cmd('hi Noice guibg='..modeColor..' guifg='..fg)
+	cmd('hi Arrow guifg='..modeColor..' guibg='..lightGrey)
+	cmd('hi MidArrow guifg='..lightGrey..' guibg='..bg)
+	cmd('hi BranchName guifg='..modeColor..' guibg='..bg)
+end
+
 function get_statusline()
-    local leftSeparator = Config.defaults.leftSeparator
-    local rightSeparator = Config.defaults.rightSeparator
-    local cool_symbol = Config.defaults.cool_symbol
-    local line_column = Config.defaults.line_column
+
+	for k,v in pairs(Config.defaults) do
+		_G[k] = Config.defaults[k]
+	end
 
 	local mode = vim.api.nvim_get_mode()['mode']
 	local extension = vim.bo.ft
 
-	ifNotFound(Config.modes, ' ')
-	ifNotFound(getFileIcon, ' ')
-	ifNotFound(Config.getModeColor, red)
+	ifNotFound(Config.mode_icons, ' ')
+	ifNotFound(Config.getFileIcon, ' ')
+	ifNotFound(Config.mode_colors, "#e27d60")
 
-	local modeIcon	= Config.modes[mode]
-	local modeColor = Config.getModeColor[mode]
-	local fileIcon	= getFileIcon[extension]
+	local modeIcon	= Config.mode_icons[mode]
+	local modeColor = Config.mode_colors[mode]
+	local fileIcon	= Config.getFileIcon[extension]
 
 	local s = '%#Noice#  '..modeIcon..' %#Arrow#'..leftSeparator
 	s = s..'%#MidArrow#'..leftSeparator
 	s = s.." %#BranchName#"..branch.. ' %M'.. "%#MidArrow#"
 
-    s = s..'%='
+	s = s..'%='
 
-    s = s..rightSeparator..'%#Arrow#'..rightSeparator..'%#Noice# '
-    s = s..fileIcon..'  '..line_column.. cool_symbol ..' '
+	s = s..rightSeparator..'%#Arrow#'..rightSeparator..'%#Noice# '
+	s = s..fileIcon..'  '..line_column.. cool_symbol ..' '
 
-	cmd('hi Noice guibg='..modeColor..' guifg=#000000')
-	cmd('hi Arrow guifg='..modeColor..' guibg='..lightGrey)
-	cmd('hi MidArrow guifg='..lightGrey)
-	cmd('hi BranchName guifg='..modeColor)
+	call_highlights(modeColor)
+
 	return s
 end
 
 return {
-	setup = Config.setup,
+	setup = setup,
 	get_statusline = get_statusline
 }
