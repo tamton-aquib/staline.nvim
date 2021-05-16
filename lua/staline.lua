f482a2c94820fc4ba79a6e0803cfc6d73996c44d
tamton-aquib <aquibjavedt007@gmail.com>
2021-05-16 14:02:39 +0530

    adding web-devicons and cleaning some sh#t code

--- lua/staline.lua
+++ lua/staline.lua
@@ -1,4 +1,5 @@
 Config = {}
+M = {}
 Tables = require('tables')
 
 local cmd = vim.api.nvim_command
@@ -7,7 +8,7 @@ Config.getFileIcon = Tables.getFileIcon
 Config.mode_colors = Tables.mode_colors
 Config.mode_icons = Tables.mode_icons
 
-function setup(opts)
+function M.setup(opts)
 	if not opts then opts = {} end
 
 	for k,v in pairs(opts) do
@@ -18,7 +19,6 @@ function setup(opts)
 
     vim.o.statusline = '%!v:lua.require\'staline\'.get_statusline()'
 end
-
 
 local lightGrey = "#303030"
 
@@ -41,37 +41,26 @@ function has_plenary(module)
 end
 has_plenary('plenary')
 
-function ifNotFound (t, d)
-  local mt = {__index = function () return d end}
-  setmetatable(t, mt)
-end
-
 function call_highlights(modeColor)
-    local fg = Config.defaults.fg
 	cmd('hi Noice guibg='..modeColor..' guifg='..fg)
 	cmd('hi Arrow guifg='..modeColor..' guibg='..lightGrey)
 	cmd('hi MidArrow guifg='..lightGrey..' guibg='..bg)
 	cmd('hi BranchName guifg='..modeColor..' guibg='..bg)
 end
 
-function get_statusline()
+function M.get_statusline()
 
 	for k,v in pairs(Config.defaults) do
 		_G[k] = Config.defaults[k]
 	end
 
 	local mode = vim.api.nvim_get_mode()['mode']
-	local extension = vim.bo.ft
+	local extension = vim.fn.expand('%:e')
+	local filename = vim.fn.expand('%:t')
 
-	local modeIcon	= Config.mode_icons[mode]
-	local modeColor = Config.mode_colors[mode]
-	-- local fileIcon	= Config.getFileIcon[extension]
-    local fileIcon, icon_highlight  = require'nvim-web-devicons'.get_icon(vim.fn.expand('%:t'), extension, options)
-
-	ifNotFound(Config.mode_icons, ' ')
-	ifNotFound(Config.getFileIcon, ' ')
-	ifNotFound(Config.mode_colors, "#e27d60")
-
+	local modeIcon	= Config.mode_icons[mode] or " "
+	local modeColor = Config.mode_colors[mode] or "#e27d60"
+    local fileIcon, icon_highlight  = require'nvim-web-devicons'.get_icon(filename, extension, {default = true})
 
 	local s = '%#Noice#  '..modeIcon..' %#Arrow#'..leftSeparator
 	s = s..'%#MidArrow#'..leftSeparator
@@ -80,14 +69,11 @@ function get_statusline()
 	s = s..'%='
 
 	s = s..'%#'..icon_highlight..'#'..fileIcon..' '.. "%#MidArrow#"
-	s = s..rightSeparator..'%#Arrow#'..rightSeparator..'%#Noice#  '..line_column.. cool_symbol ..' '
+	s = s..rightSeparator..'%#Arrow#'..rightSeparator..'%#Noice#  '..line_column..cool_symbol ..' '
 
-	call_highlights(modeColor)
+	call_highlights(modeColor, fg)
 
 	return s
 end
 
-return {
-	setup = setup,
-	get_statusline = get_statusline
-}
+return M
