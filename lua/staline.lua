ab57f64e74d7816bb3110f19d2ac651551793248
tamton-aquib <aquibjavedt007@gmail.com>
2021-05-16 19:28:29 +0530

    idk

--- lua/staline.lua
+++ lua/staline.lua
@@ -13,25 +13,24 @@ function M.setup(opts)
     vim.o.statusline = '%!v:lua.require\'staline\'.get_statusline()'
 end
 
-local lightGrey = "#303030"
-
-function get_branch()
-	local ok, nice = pcall(require, "plenary")
+local function get_branch()
+	local ok, _ = pcall(require, 'plenary')
 	if not ok then return "" end
 
-	local branch = require('plenary.job'):new({
+	local branch_name = require('plenary.job'):new({
 		command = 'git',
-		args = { 'describe', '--contains', '--all', 'HEAD' },
+		args = { 'branch', '--show-current' },
 		on_stdout = function(j, return_val)
 		return return_val
 	  end,
 	}):sync()[1]
-	return branch and ' '..branch or ""
+	return branch_name and ' '..branch_name or ""
 end
 
-local branch = get_branch('plenary')
+branch = get_branch()
 
-function call_highlights(modeColor)
+local function call_highlights(modeColor)
+	local lightGrey = "#303030"
 	cmd('hi Noice guibg='..modeColor..' guifg='..fg)
 	cmd('hi Arrow guifg='..modeColor..' guibg='..lightGrey)
 	cmd('hi MidArrow guifg='..lightGrey..' guibg='..bg)
@@ -45,29 +44,29 @@ function M.get_statusline()
 	end
 
 	local mode = vim.api.nvim_get_mode()['mode']
-	local extension = vim.fn.expand('%:e')
-	local fullpath = vim.fn.expand('%:p') or ""
-	local filename = full_path and fullpath or fullpath:match("^.+/(.+)$") or ""
-	local right_side = "%="
-	local left_side = "%="
 
 	local modeIcon	= Tables.mode_icons[mode] or " "
 	local modeColor = Tables.mode_colors[mode] or "#e27d60"
-	local fileIcon, icon_highlight  = require'nvim-web-devicons'.get_icon(filename, extension, {default = true})
 
-    if filename_position == "right" then right_side = ""
-    elseif filename_position == "left" then left_side = ""
-    elseif filename_position == "none" then filename, fileIcon = "", ""
-    else filename_position = "center" end
+	local extension = vim.fn.expand('%:e')
+	local fullpath = vim.fn.expand('%:p') or ""
+	local f_name = full_path and fullpath or fullpath:match("^.+/(.+)$") or ""
+	local f_icon, icon_highlight  = require'nvim-web-devicons'.get_icon(filename, extension, {default = true})
+
+	local right_side = "%="
+	local left_side = "%="
+
+	if filename_position == "right" then right_side = ""
+	elseif filename_position == "left" then left_side = ""
+	elseif filename_position == "none" then f_name, f_icon = "", ""
+	else filename_position = "center" end
 
 	local s = '%#Noice#  '..modeIcon..' %#Arrow#'..leftSeparator
-	s = s..'%#MidArrow#'..leftSeparator.." %#BranchName#"..branch.. ' %M'
+	..'%#MidArrow#'..leftSeparator.." %#BranchName#"..branch.. ' %M'..
 
-	s = s..left_side
-	s = s..'%#'..icon_highlight..'#  '..fileIcon.."%#BranchName#"..' '..filename.. "  %#MidArrow#"
-	s = s..right_side
+	left_side..'%#'..icon_highlight..'#  '..f_icon.."%#BranchName# "..f_name.. "  %#MidArrow#"..right_side
 
-	s = s..rightSeparator..'%#Arrow#'..rightSeparator..'%#Noice#  '..line_column..cool_symbol ..' '
+	..rightSeparator..'%#Arrow#'..rightSeparator..'%#Noice#  '..line_column..cool_symbol ..' '
 
 	call_highlights(modeColor)
 
