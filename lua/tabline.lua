75010899aa56e153fa4e7f19cd232fa6f80e2337
tamton-aquib <aquibjavedt007@gmail.com>
2021-06-10 13:19:34 +0530

    adding some styles to bufferline

--- lua/tabline.lua
+++ lua/tabline.lua
@@ -1,36 +1,34 @@
-Tabline = {}
-
-function Tabline.get_tabline()
-	local tabline = ""
+function call_tabline_colors()
 	local normal_bg = vim.api.nvim_get_hl_by_name("Normal", {})['background'] or 255
 	local normal_fg = vim.api.nvim_get_hl_by_name("Normal", {})['foreground'] or 0
 	local normal_bg_hex = string.format("%x", normal_bg)
 	local normal_fg_hex = string.format("%x", normal_fg)
 
-	vim.cmd('hi Bruh guifg='..normal_fg_hex..' guibg='..normal_bg_hex..' gui=bold')
-	vim.cmd('hi TablineSel guifg=white guibg='..normal_bg_hex..' gui=bold')
+	vim.cmd('hi StablineLeft guifg='..normal_fg_hex..' guibg='..normal_bg_hex..' gui=bold')
+	vim.cmd('hi TablineSel guifg=white guibg='..normal_bg_hex)
+end
 
-	local buffers = vim.api.nvim_list_bufs()
+function get_tabline()
+	local tabline = ""
 
-	for k,i in pairs(buffers) do
-		if vim.api.nvim_buf_is_valid(i) then
-			local edited = vim.bo.modified and "  " or ""
-			local f_name = vim.api.nvim_buf_get_name(i):match(".*%/(.+)")
+	for _, buf in pairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_valid(buf) then
+			local edited = vim.bo.modified and "" or ""
+			local f_name = vim.api.nvim_buf_get_name(buf):match(".*%/(.+)")
 			if f_name ~= nil then f_name = "  "..f_name.."  " else f_name = "" end
 
-			if f_name:match("Vim.Buffer") then f_name = "" end
-
-			if vim.api.nvim_get_current_buf() == i then
-				f_name = f_name..edited
-				tabline = tabline.."%#Bruh#".."%#TablineSel#"..f_name
+			if vim.api.nvim_get_current_buf() == buf then
+				tabline = tabline.."%#StablineLeft#|"..
+				"%#TablineSel#"..f_name.."%#StablineLeft#"..edited.." "
 			else
-				tabline = tabline.."%#TablineFill#"..f_name.. "%#TablineFill#"
+				tabline = tabline.."%#TablineFill# "..f_name.. " %#TablineFill#"
 			end
 		end
 	end
 
+	call_tabline_colors()
 	return tabline.."%#TablineFill#"
 end
 
-return Tabline
+return {get_tabline = get_tabline}
 
