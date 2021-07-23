e883b360df36f7080ce31d14f01ab8be2e0854a9
tamton-aquib <aquibjavedt007@gmail.com>
2021-07-23 17:12:18 +0530

    color invert with a dash '-'

--- lua/tables.lua
+++ lua/tables.lua
@@ -10,9 +10,9 @@ end
 Tables = {
 	sections = {
 		-- mode, left_sep, right_sep, branch, sec, filename
-		left = {'mode', 'left_double_sep', ' ', 'branch', 'filename'},
+		left = {'-mode', 'left_double_sep', ' ', 'branch', ' | ', 'filename'},
 		mid  = {'lsp'},
-		right = { 'cool_symbol','right_double_sep', 'line_column'}
+		right = { 'cool_symbol','right_double_sep', '-line_column'}
 	},
 	defaults = {
 		left_separator = "",
