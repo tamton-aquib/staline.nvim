c936d86a7a31fb6698e0ab4b2b293000b1e9d57b
tamton-aquib <aquibjavedt007@gmail.com>
2021-08-25 13:48:24 +0530

    manual merge from beta

--- lua/tables.lua
+++ lua/tables.lua
@@ -1,10 +1,10 @@
 local Tables = {}
 
 local function system_icon()
-    if vim.fn.has("win32")    == 1 then return "者"
-    elseif vim.fn.has("mac")  == 1 then return " "
-    elseif vim.fn.has("unix") == 1 then return " "
-    end
+	if vim.fn.has("win32")    == 1 then return "者"
+	elseif vim.fn.has("mac")  == 1 then return " "
+	elseif vim.fn.has("unix") == 1 then return " "
+	end
 end
 
 Tables = {
@@ -14,7 +14,7 @@ Tables = {
 		right = { 'cool_symbol','right_sep_double', '-line_column'}
 	},
 
-    lsp_symbols = {Error=" ", Information=" ", Warning=" ", Hint=""},
+	lsp_symbols = {Error=" ", Information=" ", Warning=" ", Hint=""},
 
 	defaults = {
 		left_separator = "",
@@ -31,66 +31,69 @@ Tables = {
 		lsp_client_symbol = " "
 	},
 
-    special_table = {NvimTree = {'NvimTree', ' '}, packer = {'Packer',' '}, dashboard = {'Dashboard', '  '}, help = {'Help', '龎'}},
+	special_table = {NvimTree = {'NvimTree', ' '}, packer = {'Packer',' '}, dashboard = {'Dashboard', '  '}, help = {'Help', '龎'}},
 
-    mode_colors = {
-	['n']    =  "#2bbb4f", --> "#6ed57e"
-	['v']    =  "#4799eb",
-	['V']    =  "#4799eb",
-	['i']    =  "#986fec",
-	['ic']   =  "#986fec",
-	['c']    =  "#e27d60",
-	['t']    =  "#ffd55b", --> "#" fff94c
-	['r']    =  "#fff94c",
-	['R']    =  "#fff94c",
-	['']   =  "#4799eb"
-    },
+	mode_colors = {
+		['n']    =  "#2bbb4f", --> "#6ed57e"
+		['v']    =  "#4799eb",
+		['V']    =  "#4799eb",
+		['i']    =  "#986fec",
+		['ic']   =  "#986fec",
+		['s']    =  "#986fec",
+		['S']    =  "#986fec",
+		['c']    =  "#e27d60",
+		['t']    =  "#ffd55b", --> "#" fff94c
+		['r']    =  "#fff94c",
+		['R']    =  "#fff94c",
+		['']   =  "#4799eb"
+	},
 
-    mode_icons = {
-	['n']   = ' ',
-	['v']   = ' ',
-	['V']   = ' ',
-	['i']   = ' ',
-	['ic']  = ' ',
-	['c']   = ' ',
-	['r']   = 'Prompt',
-	['t']   = ' ',
-	['R']   = ' ',
-	['']  = ' ',
+	mode_icons = {
+		['n']   = ' ',
+		['v']   = ' ',
+		['V']   = ' ',
+		['i']   = ' ',
+		['s']   = ' ',
+		['S']   = ' ',
+		['ic']  = ' ',
+		['c']   = ' ',
+		['r']   = 'Prompt',
+		['t']   = ' ',
+		['R']   = ' ',
+		['']  = ' ',
 
-	-- taken from lualine
-	['no']   = ' ',
-	['nov']  = ' ',
-	['noV']  = ' ',
-	['no'] = ' ',
-	['niI']  = ' ',
-	['niR']  = ' ',
-	['niV']  = ' ',
-    },
+		['no']   = ' ',
+		['nov']  = ' ',
+		['noV']  = ' ',
+		['no'] = ' ',
+		['niI']  = ' ',
+		['niR']  = ' ',
+		['niV']  = ' ',
+	},
 
-    file_icons = {
-	typescript         = ' ' ,
-	python             = ' ' ,
-	java               = ' ' ,
-	html               = ' ' ,
-	css                = ' ' ,
-	scss               = ' ' ,
-	javascript         = ' ' ,
-	javascriptreact    = ' ' ,
-	markdown           = ' ' ,
-	sh                 = ' ',
-	zsh                = ' ',
-	vim                = ' ',
-	rust               = ' ',
-	cpp                = ' ',
-	c                  = ' ',
-	go                 = ' ',
-	lua                = ' ',
-	conf               = ' ',
-	haskel             = ' ',
-	ruby               = ' ',
-	txt                = ' '
-    }
+	file_icons = {
+		typescript         = ' ' ,
+		python             = ' ' ,
+		java               = ' ' ,
+		html               = ' ' ,
+		css                = ' ' ,
+		scss               = ' ' ,
+		javascript         = ' ' ,
+		javascriptreact    = ' ' ,
+		markdown           = ' ' ,
+		sh                 = ' ',
+		zsh                = ' ',
+		vim                = ' ',
+		rust               = ' ',
+		cpp                = ' ',
+		c                  = ' ',
+		go                 = ' ',
+		lua                = ' ',
+		conf               = ' ',
+		haskel             = ' ',
+		ruby               = ' ',
+		txt                = ' '
+	}
 }
 
 return Tables
